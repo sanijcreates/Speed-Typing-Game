5b11aa8ff73504f160e043c734d2cc3feb52f866
Sanij Shrestha <saniz.sth123@gmail.com>
2022-10-19 23:29:48 -0400

    Adding controlled form and countWords function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    textArea : ""
+  })
+
+  function handleChange(event) {
+    setFormData({...formData, [event.target.name] : event.target.value})
+    console.log(formData)
+  }
+  //function that counts the number of words in the textarea
+  function countWords() {
+    //.split(" ") splits the string into an array of words. 
+    const arr = formData.textArea.split(" ")
+    return arr.filter(word => word != "").length;
+  }
+  countWords();
+ 
   return (
     <div className="App">
-      <h1>App</h1>
+        <h1>Some title</h1>
+        <textarea
+          onChange = {handleChange}
+          name = "textArea"
+        />
+        <h4>Time running: </h4>
+        <button>Start the game</button>
+        <h1>Word Count: {countWords()}</h1>
     </div>
   );
 }
